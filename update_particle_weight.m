f287fffc18f60ef812664d2af838e0fcc4214b4c
ron <rpicard92@gmail.com>
2019-04-30 11:30:43 -0500

    repo cleanups and data log updates

--- update_particle_weight.m
+++ update_particle_weight.m
@@ -1,24 +1,11 @@
-function [particles, new_weight] = update_particle_weight(nbr_of_states,predicted_velocity,measured_v,particles,particle_index,velocity_sigma)
+function [particles, new_weight] = update_particle_weight(nbr_of_states,predicted_velocity,measured_v,particles,particle_index)
     
     % for each state
     for state = 1:nbr_of_states
+
         % calculate the likelihood of measured value
         new_weight = abs(measured_v-predicted_velocity);
 
-%        new_weight = abs(measured_v-predicted_velocity)/velocity_sigma;
-
-%         diff = measured_v - predicted_velocity;
-%         if(diff > 0)
-%             upper = normcdf(measured_v,predicted_velocity,velocity_sigma);
-%             lower = normcdf(measured_v-2*diff,predicted_velocity,velocity_sigma);
-%             new_weight = 1 - (upper-lower);
-%         elseif(diff < 0)
-%             upper = normcdf(measured_v+2*diff,predicted_velocity,velocity_sigma);
-%             lower = normcdf(measured_v,predicted_velocity,velocity_sigma);
-%             new_weight = 1 - (upper-lower);
-%         else
-%             new_weight = 1;
-%         end
         % update weight
         particles(state, particle_index, 2) = new_weight;
     end
